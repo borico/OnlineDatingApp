c272d06f1711528c35b99bbb90847c4140f928fd
borico <bflash61@gmail.com>
2020-09-14 15:48:56 +0300

    push changes to heroku

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://onlinedatingapp:Istanbul2018@onlinedatingapp.gprbo.mongodb.net/OnlineDatingApp?retryWrites=true&w=majority',
+    FacebookAppID: '1020157478421969',
+    FacebookAppSecret: '4034d606ed50b73ca3299ddf2f14ffb4',
+    GoogleClientID: '527879059341-htjbjbfgf5ped42v996hospq95f890ir.apps.googleusercontent.com',
+    GoogleClientSecret: '2UVg1owUm37H1xzv4q3oJgEt'
+};
